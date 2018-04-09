6db90b1d989be8c5b1b12d9cd990a6e3f7b45f64
HyunminKo <hyunmin.ko4578@gmail.com>
2018-04-09 16:07:26 +0900

    Fibonacci call cal

--- CodingPratice/CodingPratice/Inflearn/DynamicProgramming/Fib.cpp
+++ CodingPratice/CodingPratice/Inflearn/DynamicProgramming/Fib.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <cstdio>
+#include <vector>
 
 using namespace std;
+
+vector<int> call;
+
 int f[100];
+
 int fib(int n){
     f[1] = f[2] = 1;
     for( int i = 3; i<=n;i++)
         f[i] = f[i-1] + f[i-2];
     return f[n];
 }
+int fibonacci(int n){
+    call[n]++;
+    if(n < 2) return n;
+    return fibonacci(n-2) + fibonacci(n-1);
+}
 int main(){
-    printf("%d\n",fib(10));
+    int N = 10;
+    call.resize(N+1);
+    printf("F(%d): %d\n", N, fibonacci(N));
+    puts("호출 횟수");
+    for(int i=0; i<=N; i++)
+        printf("F(%d): %d회 호출\n", i, call[i]);
     return 0;
 }
 
