63cc58299b5ea402beec6e73bf8dfa4a9a06a6ba
HyunminKo <hyunmin.ko4578@gmail.com>
2018-04-10 20:53:22 +0900

    01tile dp

--- CodingPratice/CodingPratice/DynamicProgramming/Tile01.cpp
+++ CodingPratice/CodingPratice/DynamicProgramming/Tile01.cpp
@@ -1,1 +1,15 @@
-
+#include <cstdio>
+using namespace std;
+const int MAX_N = 1000001;
+const int divideV = 15746;
+int N;
+int dp[MAX_N];
+int main(){
+    scanf("%d",&N);
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i <= N; i++){
+        dp[i] = (dp[i-2] + dp[i-1])%divideV;
+    }
+    printf("%d\n",dp[N]);
+}
