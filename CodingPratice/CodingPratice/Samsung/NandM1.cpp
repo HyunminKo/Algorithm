166447285e2a4d4fece13f38296d3e8e69cd1d09
HyunminKo <hyunmin.ko4578@gmail.com>
2018-04-14 21:35:16 +0900

    Permutation NandM 1,2

--- CodingPratice/CodingPratice/Samsung/NandM1.cpp
+++ CodingPratice/CodingPratice/Samsung/NandM1.cpp
@@ -4,20 +4,13 @@ using namespace std;
 int N,M;
 bool visited[9];
 int res[9];
-bool check(){
-    for(int i = 0 ; i < M-1; i++){
-        if(res[i]>=res[i+1]) return false;
-    }
-    return true;
-}
+
 void dfs(int n){
     if(n==M){
-        if(check()){
-            for(int i = 0 ; i < M; i++){
-                printf("%d ",res[i]);
-            }
-            printf("\n");
+        for(int i = 0 ; i < M; i++){
+            printf("%d ",res[i]);
         }
+        printf("\n");
         return;
     }
     for(int i = 0 ; i < N; i++){
--- CodingPratice/CodingPratice/Samsung/NandM2.cpp
+++ CodingPratice/CodingPratice/Samsung/NandM2.cpp
@@ -1,9 +1,37 @@
-//
-//  NandM2.cpp
-//  CodingPratice
-//
-//  Created by 고현민 on 14/04/2018.
-//  Copyright © 2018 Ko. All rights reserved.
-//
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+int N,M;
+bool visited[9];
+int res[9];
+bool check(){
+    for(int i = 0 ; i < M-1; i++){
+        if(res[i]>=res[i+1]) return false;
+    }
+    return true;
+}
+void dfs(int n){
+    if(n==M){
+        if(check()){
+            for(int i = 0 ; i < M; i++){
+                printf("%d ",res[i]);
+            }
+            printf("\n");
+        }
+        return;
+    }
+    for(int i = 0 ; i < N; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            res[n] = i + 1;
+            dfs(n+1);
+            visited[i] = false;
+        }
+    }
+    
+}
+int main(){
+    scanf("%d %d",&N,&M);
+    dfs(0);
+}
 
-#include <stdio.h>
