548ea494351e98bcd312086e2b3b70ba6dda8188
HyunminKo <hyunmin.ko4578@gmail.com>
2018-09-02 19:30:18 +0900

    Init the new algorithm folder

---
+++ AlgorithmicProblemSolvingStrategies/AlgorithmicProblemSolvingStrategies/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmicProblemSolvingStrategies
+//
+//  Created by 고현민 on 02/09/2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
