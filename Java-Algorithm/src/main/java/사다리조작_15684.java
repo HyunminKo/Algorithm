bd490fbb9d556aa71aac6a41cb1712dee669183e
HyunminKo <hyunmin.ko4578@gmail.com>
2018-11-29 11:41:42 +0900

    Init java repo

---
+++ Java-Algorithm/src/main/java/사다리조작_15684.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class 사다리조작_15684 {
+    public static void main(String[] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        try {
+            int test = br.read();
+            bw.write(test+"\n");
+            bw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
